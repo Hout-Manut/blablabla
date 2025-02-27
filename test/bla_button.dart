ae15b56a5f2ea740fc1a86effa98c1c4cbcda892
Hout Manut <Huotmanut00@gmail.com>
2025-02-28 01:58:29 +0700

    Move test to the root directory

--- test/bla_button.dart
+++ test/bla_button.dart
@@ -1,4 +1,4 @@
-import 'package:blablabla/widgets/inputs/bla_button.dart';
+import 'package:blablabla/widgets/actions/bla_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ test/bla_location_picker.dart
@@ -0,0 +1,7 @@
+import 'package:blablabla/widgets/inputs/bla_location_picker.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: Center(child: BlaLocationPicker(),),),));
+}
